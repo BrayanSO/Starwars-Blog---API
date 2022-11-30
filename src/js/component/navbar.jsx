2df777c9cda128665e2e95a16e9c7d0e583bb526
BrayanSO <briansolera@gmail.com>
2022-11-30 02:56:52 +0000

    primera version

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+	return (
+		<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+  <Link to="/">
+    <a class="navbar-brand" href="#">🌟</a>
+	</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+		<Link to="/demo">
+          <a class="nav-link active" aria-current="page" href="#">Naves</a>
+		  </Link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+		
+	);
+};
+
---
+++ src/js/views/demo.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext.js";
+
+import "../../styles/demo.css";
+
+export const Demo = () => {
+	return (
+		<div className="container flex-row">
+		<h1>Naves</h1>
+			<Link to="/">
+				<button className="btn btn-primary row">Back home</button>
+			</Link>
+		</div>
+	);
+};
